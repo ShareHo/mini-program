a80882d08128a8aa61eaa9199174f61132607198
share_ho <hezixuan@52tt.com>
2023-09-04 22:54:34 +0800

    feat: 上线字段修改

--- miniprogram/utils/env.js
+++ miniprogram/utils/env.js
@@ -1,7 +1,7 @@
 // 是否测试api使用
 export const isTest = false;
-// 开发时控制审核页面和正式页面切换
-export const isOnline = false;
+// 控制审核页面和正式页面切换，每次上线前改为true
+export const isOnline = true;
 // 每次上线前字段轮换使用
 export const reviewCode = 'reviewCode1';
 // export const reviewCode = 'reviewCode2';
